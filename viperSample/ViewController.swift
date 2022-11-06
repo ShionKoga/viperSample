59bef2e9c6fda6e598cdd64afe03710bd2dc6c2d
Shion Koga <shion_koga@mail.toyota.co.jp>
2022-11-06 16:46:02 +0900

    delete storybord

--- viperSample/ViewController.swift
+++ viperSample/ViewController.swift
@@ -1,17 +1,10 @@
-//
-//  ViewController.swift
-//  viperSample
-//
-//  Created by 14inchの史苑 on 2022/11/06.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
     }
 
 
